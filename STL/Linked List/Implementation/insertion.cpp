a17839cedf8f5642cdd3a5e25db62d24e5b4e35b
ramjirathore <ramjirathore2802@gmail.com>
2020-11-26 03:28:40 +0530

    insert in middle

--- STL/Linked List/Implementation/insertion.cpp
+++ STL/Linked List/Implementation/insertion.cpp
@@ -52,6 +52,38 @@ void insertAtTail(node* &head, int d) {
 	tail -> next = n;
 }
 
+int length(node *head) {
+	int len = 0;
+	while(head!=NULL) {
+		head = head -> next;
+		len++;
+	}
+
+	return len;
+}
+
+void insertInMiddle(node* &head, int data, int p) {
+
+	if(head == NULL) {
+		insertAtHead(head, data);
+	} else if(p>length(head)) {
+		insertAtTail(head, data);
+	} else {
+		int jump = 1;
+		node *temp = head;
+
+		// reach the correct position
+		while(jump <= p - 1) {
+			temp = temp -> next;
+			jump++;
+		}
+
+		node *n = new node(data);
+		n->next = temp->next;
+		temp->next = n;
+	}
+} 
+
 
 // passing head pointer by value
 void  print(node *head) {
@@ -73,6 +105,7 @@ int main() {
 
 	print(head);
 
+	insertInMiddle(head, 4, 3);
 	insertAtTail(head, 7);
 
 	print(head);
