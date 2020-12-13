4fd48cc76e2f1e3e6a82795c7c42c866964532a1
ramjirathore <ramjirathore2802@gmail.com>
2020-12-13 22:08:49 +0530

    basic approach

---
+++ SDE Sheet/Arrays/missing_and_repeating_number.cpp
@@ -0,0 +1,13 @@
+// PROBLEM - 
+// Given an unsorted array Arr of size N of positive integers. 
+// One number 'A' from set {1, 2, …N} is missing and one number 'B' occurs twice in array. 
+// Find these two numbers.
+
+// LINK - https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/
+// SOLUTION - https://www.youtube.com/watch?v=5nMGY4VUoRY&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=3
+
+// Brute force - Sort and then find => O(nlogn+n) time and O(1) space
+
+// Hashmap or frequency array - O(n) time and O(n) space
+
+
