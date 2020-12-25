83ca71cf55b3bb9337aa77a17e95e6dbe9647640
ramjirathore <ramjirathore2802@gmail.com>
2020-12-25 15:35:32 +0530

    Gap method - shell sort is intution

---
+++ Practice/GFG/merge_two_sorted_arrays_without_extra_space.cpp
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+int nextGap(int gap)
+{
+    if (gap <= 1)
+        return 0;
+    return (gap / 2) + (gap % 2);
+}
+
+void merge(int arr1[], int arr2[], int n, int m)
+{ 
+    int gap = nextGap(n+m);
+    int i, j;
+
+    while(gap>0) {
+        // comparing in first array
+        for(i=0; i + gap < n; i++) {
+            if(arr1[i] > arr1[i+gap]) {
+                swap(arr1[i], arr1[i+gap]);
+            }
+        }
+        
+        // comparing in both arrays
+        for(j = gap > n ? gap - n : 0; i<n && j<m; i++, j++) {
+            if(arr1[i] > arr2[j]) {
+                swap(arr1[i], arr2[j]);
+            }
+        }
+        
+        //comparing in second array
+        if(j<m) {
+                for(j=0; j+gap < m; j++) {
+                    if(arr2[j] > arr2[j+gap]) {
+                        swap(arr2[j], arr2[j+gap]);
+                }
+            }
+        }
+        
+        gap = nextGap(gap);
+    }
+    
+} 
+
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    int arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    
+	    merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            printf("%d ", arr1[i]); 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    printf("%d ", arr2[i]); 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
