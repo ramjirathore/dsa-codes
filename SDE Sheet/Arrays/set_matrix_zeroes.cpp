ea4efbfbf64d93d25d42b326aef85f66d4ef01dc
ramjirathore <ramjirathore2802@gmail.com>
2020-12-26 19:00:37 +0530

    optimal - built aux array inside given matrix

--- SDE Sheet/Arrays/set_matrix_zeroes.cpp
+++ SDE Sheet/Arrays/set_matrix_zeroes.cpp
@@ -79,3 +79,43 @@ public:
        }
     }
 };
+
+
+
+
+
+// OPTIMAL - O((n*m)) time and O(1) space
+// Improving the better approach 
+// we will use the given 2D array for storing the aux array
+// col[0] and row[0] 
+// plus imp -  we will use col = true and a[0][0]
+// so that both col[0] and row[0] don't simultaneouosly become zero when one gets affected
+
+// Then we will set values to zero acc while iterating in reverse (#imp)
+
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+       int row = matrix.size(), col = matrix[0].size(); 
+        bool col0 = true;
+        
+        for(int i=0; i<row; i++) {
+            if(matrix[i][0] == 0) col0 = false; // if any zero in first col 
+            for(int j=1; j<col; j++) {
+                if(matrix[i][j] == 0) {
+                  matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        
+       for(int i=row-1; i>=0; i--) {
+           for(int j = col-1; j>=1; j--) {
+               if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                   matrix[i][j] = 0;
+               }
+           }
+         if(col0 == false) matrix[i][0] = 0;
+       }
+    }
+};
