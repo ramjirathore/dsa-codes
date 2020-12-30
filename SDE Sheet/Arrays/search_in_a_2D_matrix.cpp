7a68cd6c100b8d12f7257931db4dd380a15adea4
ramjirathore <ramjirathore2802@gmail.com>
2020-12-30 22:04:40 +0530

    apply binary search assuming it 1D array

--- SDE Sheet/Arrays/search_in_a_2D_matrix.cpp
+++ SDE Sheet/Arrays/search_in_a_2D_matrix.cpp
@@ -69,6 +69,36 @@ public:
 
 
 
+// OPTIMAL - O(log(n*m)) time and O(1) space
+// 2nd point tells that the whole matrix if put in a single array
+// Then it will be sorted
+// we will exhaust this by using binary search
+// But without any extra space
+// #imp - we calc row and col of mid using mid/m and mid%m
 
-
-// OPTIMAL
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(!matrix.size()) return false;
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+       int low = 0;
+       int high = n*m - 1;
+        
+        while(low<=high) {
+            int mid = low + (high - low)/2;
+            
+            if(matrix[mid/m][mid%m] == target) {
+                return true;
+            } else if(matrix[mid/m][mid%m] < target) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+};
