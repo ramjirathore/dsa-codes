701844987ed9532530d44e5b965d7f3081d27d46
ramjirathore <ramjirathore2802@gmail.com>
2021-01-27 22:05:27 +0530

    brute force - traverse all subarrays and find max length

--- SDE Sheet/Hashing/largest_subarray_with_zero_sum.cpp
+++ SDE Sheet/Hashing/largest_subarray_with_zero_sum.cpp
@@ -1,1 +1,13 @@
-// PROBLEM -
+// PROBLEM - 
+
+// Given an array having both positive and negative integers. 
+// The task is to compute the length of the largest subarray with sum 0.
+
+// Link - https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+// Solution - https://www.youtube.com/watch?v=xmguZ6GbatA&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=22
+
+
+// BRUTE FORCE - O(n^3) time and O(1) space
+// Simply check for all the subarrays where sum becomes 0
+// Constantly update the max length of such array 
+// return the answer in the end 
