bc1dc01afa48c0774059ff07ab5dba311c600597
ramjirathore <ramjirathore2802@gmail.com>
2021-01-27 22:07:22 +0530

    optimal approach - check if sum exits in hashmap

--- SDE Sheet/Hashing/largest_subarray_with_zero_sum.cpp
+++ SDE Sheet/Hashing/largest_subarray_with_zero_sum.cpp
@@ -11,3 +11,40 @@
 // Simply check for all the subarrays where sum becomes 0
 // Constantly update the max length of such array 
 // return the answer in the end 
+// Simple code, write it yourself:P
+
+
+
+// OPTIMAL - O(nLogn) time and O(n) space
+// n due to traversal and logn for worst case hit in checking hashmap
+
+// Best is to watch video to understand clearly intution
+int maxLen(int A[], int n)
+{
+    unordered_map<int, int> mp;
+    
+    
+    int sum = 0;
+    int maxi = 0;
+    
+    for(int i=0; i<n; i++) {
+        sum += A[i];
+       
+        // if till now sum is zero then length will be current index + 1
+        if(sum == 0) {
+            maxi = i + 1;
+        } 
+        else {
+            // else check if sum exists and then means all the elements after that sum is making sum same
+            // which implies that this patch sums up to zero, so update max length
+            if(mp.find(sum) != mp.end()) {
+                maxi = max(maxi, i - mp[sum]);
+            } else {
+                // else if sum is not there in hash table then add it 
+                mp[sum] = i;
+            }
+        }
+    }
+    
+    return maxi;
+}
