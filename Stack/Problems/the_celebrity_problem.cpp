66d560f272cfd4bdbf8d88ef420203f8fc3245a2
ramjirathore <ramjirathore2802@gmail.com>
2021-07-27 01:49:31 +0530

    better approach - use given information and find potential using stack

--- Stack/Problems/the_celebrity_problem.cpp
+++ Stack/Problems/the_celebrity_problem.cpp
@@ -8,6 +8,14 @@
 // LINK - https://practice.geeksforgeeks.org/problems/the-celebrity-problem/1#
 
 
+// Prove that there cannot be two celebrities - 
+// Assume if 2 is celebrity then 2 is known by everyone and he doesn't everyone
+// For another celebrity to exist he must be known by everyone but since 2 is already celebrity
+// He doesn't know this new person => hence proved by contradiction that there will be only 1 celebrity
+// Not more than that
+
+
+
 // Simple graph approach - O(N*N) time and O(N) space
 // Find indeg and outdegree
 // For a celeb outdeg must be zero and indeg must be n-1
@@ -38,3 +46,69 @@ class Solution
         return -1;
     }
 };
+
+
+// BETTER (Stack Approach) - O(N) time and O(N) space
+// For this approach we will use given information
+// If i knows j then for sure i is not celebrity but j could be
+// Similarly, If j knows i then for j is not celebrity but i could be
+
+// So using we find our potential candidate 
+// But we need to confirm it (Why ? since we did not all other i js for this candidate)
+// So if anyone knows our candidate or our candidate know anyonne then he is not celeb
+// Otherwise for sure he is
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int> s;
+        
+        for(int i=0; i<n; i++) {
+            s.push(i);
+        }
+        
+        while(s.size() >= 2) { 
+            // choose two persons 
+            int i = s.top();
+            s.pop();
+            
+            int j = s.top();
+            s.pop();
+            
+            if(M[i][j] == 1) {
+                // i knows j => i not celeb j could be 
+                // so push it back
+                s.push(j);
+            } else {
+                // i doesn't know j => j is not celeb i could be
+                // so push it back
+                s.push(i);
+            }
+        }
+        
+        if(s.empty()) { // if stack empty means no celeb
+            return -1;
+        }
+        
+        int candidate = s.top();
+        s.pop();
+        
+        
+        // Verfiy if its celebrity or not
+        for(int i=0; i<n; i++) {
+            // if candidate is not himself
+            // and if he candidate know i or i doesn't candidate then for sure he is not celeb
+            if(candidate != i and (M[candidate][i] == 1 or M[i][candidate] == 0)) {
+                return -1;
+            }
+        }
+        
+        // else he is return the candidate
+        return candidate;
+    }
+};
+
+
