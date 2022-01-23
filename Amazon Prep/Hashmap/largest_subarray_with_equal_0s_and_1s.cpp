79191a2cfb2998907fa51f5a0d7e361d2ce86d71
ramjirathore <ramjirathore2802@gmail.com>
2022-01-23 19:01:21 +0530

    brute: just make all subarrays, and then update maxlen whenever sum is zero

---
+++ Amazon Prep/Hashmap/largest_subarray_with_equal_0s_and_1s.cpp
@@ -0,0 +1,40 @@
+// PROBLEM - 
+// Given an array of 0s and 1s. Find the length of the largest subarray with equal number of 0s and 1s.
+
+// LINK - https://practice.geeksforgeeks.org/problems/largest-subarray-of-0s-and-1s/1/
+
+// BRUTE FORCE - O(n^2) time and O(1) space
+// We add -1 instead of 0 and 1 in place of 1, then we just check for all subarrays sum to be zero 
+// because when sum is zero => the zeroes and ones are equal in count
+
+int findSubArray(int arr[], int n)
+{
+    int sum = 0;
+    int maxsize = -1, startindex;
+
+    // Pick a starting point as i
+    for (int i = 0; i < n - 1; i++) {
+        sum = (arr[i] == 0) ? -1 : 1;
+
+        // Consider all subarrays starting from i
+        for (int j = i + 1; j < n; j++) {
+            (arr[j] == 0) ? (sum += -1) : (sum += 1);
+
+            // If this is a 0 sum subarray, then
+            // compare it with maximum size subarray
+            // calculated so far
+            if (sum == 0 && maxsize < j - i + 1) {
+                maxsize = j - i + 1;
+                startindex = i;
+            }
+        }
+    }
+    if (maxsize == -1)
+        cout << "No such subarray";
+    else
+        cout << startindex << " to "
+             << startindex + maxsize - 1;
+
+    return maxsize;
+}
+
